dd72b5e26140e8df6a188834d71f305035d79835
BenSanti <bensonchineduotti@gmail.com>
2023-06-26 17:41:28 -0700

    added unsigned int convert

--- main.h
+++ main.h
@@ -8,5 +8,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 void putchar_dec(int num, int *len);
 void conv_binary(unsigned int n, int *len);
+void putchar_uns(unsigned int num, int *len);
 
 #endif /* MAIN_H */
