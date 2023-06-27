7fa7aa9fb4fb5192611463f220416fa7d327feed
iAbas <abasiekemeetim20@gmail.com>
2023-06-27 21:56:06 +0100

    a function used to accumulate dat in a buffer before writing

---
+++ buffer_write.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * Buffer_write - A function used accumulate data in a
+ * buffer before writing
+ * @data: Type of data
+ * @size: size of data
+ *
+ * Return: nothing
+ */
+#define BUF_SIZ 1024
+
+void buffer_write(int fd, const char *data, size_t size)
+{
+    char buffer[BUF_SIZ];
+    size_t buffer_pos = 0;
+
+    if (buffer_pos + size < BUF_SIZ)
+    {
+        memcpy(buffer_pos + buffer, data, size);
+        buffer_pos += size;
+    }
+    else
+    {
+        write(fd, buffer, buffer_pos);
+        memcpy(buffer, data, size);
+        buffer_pos = size;
+    }
+}
+
+/**
+ * Buffer_flush - A function used to flush a buffer
+ * @fd: file descriptor
+ *
+ * Return: nothing
+ */
+void buffer_flush(int fd)
+{
+    char buffer[BUF_SIZ];
+    size_t buffer_pos = 0;
+
+    write(fd, buffer, buffer_pos);
+    buffer_pos = 0;
+}
