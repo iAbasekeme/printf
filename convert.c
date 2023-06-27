dbbc9cc71534fe7afe52852f02e95d356ee30482
BenSanti <bensonchineduotti@gmail.com>
2023-06-26 17:07:42 -0700

    added bin_conv and putchar_dec as convert func

---
+++ convert.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * conv_binary - This function converts unsigned int to binary.
+ * @n: unsigned int
+ * @len: pointer to int
+ *
+ * Return: none
+ */
+
+void conv_binary(unsigned int n, int *len)
+{
+	if (n > 1)
+		conv_binary(n >> 1, len);
+
+	_putchar((n & 1) + '0');
+	*len += 1;
+}
+
+/**
+ * putchar_dec - output decimal using putchar.
+ * @num: int
+ * @len: pointer to int
+ *
+ * Return: none.
+ */
+
+void putchar_dec(int num, int *len)
+{
+    if (num < 0) {
+        _putchar('-');
+        *len += 1;
+        num = -num;
+    }
+    if (num > 9)
+        putchar_dec(num / 10, len);
+
+    _putchar(num % 10 + '0');
+    *len += 1;
+}
