f3046dabe3191ff7afe823d653cfca612a90668b
BenSanti <bensonchineduotti@gmail.com>
2023-06-28 01:52:02 -0700

    fixed betty issue

--- buffer_write.c
+++ buffer_write.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#define BUF_SIZE 1024
 
 /**
  * buffer_write - A function used accumulate data in a
  * buffer before writing
  * @data: Type of data
  * @size: size of data
+ * @fd: int
  *
  * Return: nothing
  */
-#define BUF_SIZ 1024
 
 void buffer_write(int fd, const char *data, size_t size)
 {
-char buffer[BUF_SIZ];
-size_t buffer_pos = 0;
+	char buffer[BUF_SIZ];
+	size_t buffer_pos = 0;
 
-if (buffer_pos + size < BUF_SIZ)
-{
-memcpy(buffer_pos + buffer, data, size);
-buffer_pos += size;
-}
-else
-{
-write(fd, buffer, buffer_pos);
-memcpy(buffer, data, size);
-buffer_pos = size;
-}
+	if (buffer_pos + size < BUF_SIZ)
+	{
+		memcpy(buffer_pos + buffer, data, size);
+		buffer_pos += size;
+	}
+	else
+	{
+		write(fd, buffer, buffer_pos);
+		memcpy(buffer, data, size);
+		buffer_pos = size;
+	}
 }
 
 /**
@@ -36,11 +37,12 @@ buffer_pos = size;
  *
  * Return: nothing
  */
+
 void buffer_flush(int fd)
 {
-char buffer[BUF_SIZ];
-size_t buffer_pos = 0;
+	char buffer[BUF_SIZ];
+	size_t buffer_pos = 0;
 
-write(fd, buffer, buffer_pos);
-buffer_pos = 0;
+	write(fd, buffer, buffer_pos);
+	buffer_pos = 0;
 }
